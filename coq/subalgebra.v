6e5cb68250447d06feff21626e6f1cf11a6cc032
Guillaume Combette <guillaume.combette@ens-lyon.fr>
2018-01-25 10:55:37 +0100

    Fill the holes introduced in the previous commit.

--- coq/subalgebra.v
+++ coq/subalgebra.v
@@ -380,7 +380,22 @@ Section Quotients.
   Proof. move=> x y. by rewrite /par2_eqv [[set y; x]]setUC [y == x]eq_sym. Qed.
 
   Definition par2_eqv_trans : transitive par2_eqv.
-  Admitted.
+  Proof.
+    move=> y x z. rewrite /par2_eqv.
+    case/altP: (x =P y) => [<-//|xNy/=]. case/altP: (y =P z) => [<-->//|yNz/=].
+    case/boolP: ((g_in != g_out :> G1) && (g_in != g_out :> G2)).
+    - rewrite !inE -(implyNb (x == z)).
+      case/andP=> /negbTE iNo1 /negbTE iNo2 Hxy Hyz. apply/implyP=> xNz.
+      move: Hxy xNy. rewrite 2!eqEcard 2!subUset 4!sub1set !inE.
+      case/orP => /andP[]/andP[] /orP[]/eqP ? /orP[]/eqP ? _; subst=> // _.
+      all: move: Hyz xNz yNz; rewrite 2!eqEcard 2!subUset 4!sub1set !inE.
+      all: rewrite ?eqxx ?sum_eqE ?[g_out == g_in]eq_sym ?iNo1 ?iNo2 ?orbT ?orbF /=.
+      all: case/andP => /orP[]/eqP-> _; by rewrite !eqxx.
+    - rewrite -implyNb => _ Hxy Hyz. apply/implyP=> xNz.
+      rewrite subUset !sub1set. apply/andP; split.
+      + apply: (subsetP Hxy). by rewrite !inE eqxx.
+      + apply: (subsetP Hyz). by rewrite !inE eqxx.
+  Qed.
 
   Canonical par2_eqv_equivalence :=
     EquivRel par2_eqv par2_eqv_refl par2_eqv_sym par2_eqv_trans.
@@ -400,7 +415,42 @@ Section Quotients.
     end.
 
   Lemma par2_equiv_of : par2_eqv =2 equiv_of par2_eq.
-  Admitted.
+  Proof.
+    move=> x y. apply/idP/idP.
+    - rewrite /par2_eqv. case/altP: (x =P y) => [<- _|xNy]/=; first exact: connect0.
+      case: ifP => [_|/negbT].
+      + rewrite !inE 2!eqEcard 2!subUset 4!sub1set !inE => H.
+        case/orP: H xNy => /andP[]/andP[]/orP[]/eqP->/orP[]/eqP-> _.
+        all: rewrite ?eqxx // ?[equiv_of _ (inr _) _]equiv_of_sym => _.
+        all: apply: sub_equiv_of; by rewrite /par2_eq 2!eqxx.
+      + rewrite negb_and 2!negbK. case/orP=> [/eqP Eio1|/eqP Eio2].
+        * rewrite -Eio1 setUid => Exy.
+          have Rio2 : equiv_of par2_eq (inr g_out) (inr g_in).
+          { apply: (@equiv_of_trans _ _ (inl g_in)); first rewrite equiv_of_sym Eio1;
+            by apply: sub_equiv_of; rewrite /par2_eq !eqxx. }
+          move: Exy xNy; rewrite subUset 2!sub1set !inE -2!orbA.
+          case/andP=> /or3P[]/eqP-> /or3P[]/eqP->.
+          all: rewrite ?eqxx // ?[equiv_of _ _ (inl _)]equiv_of_sym => _.
+          all: try solve [apply: (@sub_equiv_of _ _ (inl _));
+                          by rewrite /par2_eq -?Eio1 !eqxx].
+          by rewrite equiv_of_sym.
+        * rewrite -setUA -Eio2 setUid => Exy.
+          have Rio2 : equiv_of par2_eq (inl g_out) (inl g_in).
+          { apply: (@equiv_of_trans _ _ (inr g_out)); last rewrite equiv_of_sym -Eio2;
+            by apply: sub_equiv_of; rewrite /par2_eq !eqxx. }
+          move: Exy xNy; rewrite subUset 2!sub1set !inE -2!orbA.
+          case/andP=> /or3P[]/eqP-> /or3P[]/eqP->.
+          all: rewrite ?eqxx // ?[equiv_of _ (inr _) _]equiv_of_sym => _.
+          all: try solve [apply: (@sub_equiv_of _ _ (inl _));
+                          by rewrite /par2_eq -?Eio2 !eqxx].
+          by rewrite equiv_of_sym.
+    - case/connectP=> p. elim: p x; first by [move=> x _ ->; exact: par2_eqv_refl].
+      move=> /= a p IH x /andP[x_a p_path p_last].
+      have {p_path p_last} : par2_eqv a y by exact: IH. apply: par2_eqv_trans.
+      move: x a x_a => [x|x] [a|a] //=; first rewrite orbF.
+      all: case/orP=> /andP[/eqP-> /eqP->].
+      all: by rewrite ?par2_eqv_io // par2_eqv_sym par2_eqv_io.
+  Qed.
 
   Definition par2 := 
     {| graph_of := merge (union G1 G2) par2_eqv ;
@@ -446,7 +496,16 @@ Section Quotients.
   Proof. move=> x y. by rewrite /seq2_eqv [[set y; x]]setUC [y == x]eq_sym. Qed.
 
   Definition seq2_eqv_trans : transitive seq2_eqv.
-  Admitted.
+  Proof.
+    move=> y x z. rewrite /seq2_eqv.
+    case/altP: (x =P y) => [<-//|/= xNy Exy].
+    case/altP: (y =P z) => [<-|/= yNz Eyz]; first by rewrite Exy.
+    case/altP: (x =P z) => //= xNz.
+    have := eq_refl #|[set inl (@g_out G1); inr (@g_in G2)]|.
+    rewrite -{1}(setUid [set _; _]) -{1}(eqP Exy) -{1}(eqP Eyz).
+    rewrite set2C -setUUr (cardsU1 y) !cards2 !inE.
+    by rewrite (eq_sym y x) negb_or xNy yNz xNz.
+  Qed.
 
   Canonical seq2_eqv_equivalence :=
     EquivRel seq2_eqv seq2_eqv_refl seq2_eqv_sym seq2_eqv_trans.
@@ -458,7 +517,19 @@ Section Quotients.
     [rel a b | (a == inl g_out) && (b == inr g_in)].
 
   Lemma seq2_equiv_of : seq2_eqv =2 equiv_of seq2_eq.
-  Admitted.
+  Proof.
+    move=> x y. apply/idP/idP.
+    - case/altP: (x =P y) => [<- _|xNy]; first exact: connect0.
+      rewrite /seq2_eqv (negbTE xNy) /= eqEcard subUset !sub1set !inE => /andP[H _].
+      case/andP: H xNy => /orP[]/eqP-> /orP[]/eqP->; rewrite ?eqxx // => _.
+      + apply: sub_equiv_of. by rewrite /seq2_eq/= !eqxx.
+      + rewrite equiv_of_sym. apply: sub_equiv_of. by rewrite /seq2_eq/= !eqxx.
+    - case/connectP=> p. elim: p x; first by [move=> x _ ->; exact: seq2_eqv_refl].
+      move=> /= a p IH x /andP[x_a p_path p_last].
+      have {p_path p_last} : seq2_eqv a y by exact: IH. apply: seq2_eqv_trans.
+      case/orP: x_a => /andP[/eqP-> /eqP->];
+      last rewrite seq2_eqv_sym; exact: seq2_eqv_io.
+  Qed.
 
   Definition seq2 :=
     {| graph_of := merge (union G1 G2) seq2_eqv ;
