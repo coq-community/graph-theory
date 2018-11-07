61eaf624479d09269a3c005b5939b05f8e293766
Christian Doczkal <christian.doczkal@ens-lyon.fr>
2018-11-07 16:54:42 +0100

    add/group set_tac rules

--- coq/set_tac.v
+++ coq/set_tac.v
@@ -3,21 +3,45 @@ Require Import preliminaries.
 
 (** * Simple Experimental Tactic for finite sets *)
 
+Section SetTac.
+Variables (T : finType) (A B C : {set T}).
+
+Lemma set_tac_subUl: A :|: B \subset C -> A \subset C /\ B \subset C.
+Proof. rewrite subUset. by case/andP. Qed.
+
+Lemma set_tac_subIr: A \subset B :&: C -> A \subset B /\ A \subset C.
+Proof. rewrite subsetI. by case/andP. Qed.
+
+Lemma set_tac_subIl x : 
+  x \in A -> x \in B -> A :&: B \subset C -> x \in C.
+Proof. move => xA xB /subsetP. apply. by rewrite inE xA xB. Qed.
+
+End SetTac.
+
 Lemma setIPn (T : finType) (A B : {set T}) (x:T) : 
   reflect (x \notin A \/ x \notin B) (x \notin A :&: B).
 Proof. rewrite !inE negb_and. exact: orP. Qed.
 
 Ltac notHyp b := match goal with [_ : is_true b |- _] => fail 1 | _ => idtac end.
 
+Ltac extend H T := notHyp H; have ? : H by T.
+
+Ltac convertible A B := assert_succeeds (assert (A = B) by reflexivity).
+
 (** NOTE: since Ltac is untyped, we need to provide the coercions
 usually hidden, e.g., [is_true] or [SetDef.pred_of_set] *)
 
+  (* lazymatch goal with *)
+  (* | [H : is_true (?x \in ?A), S : is_true (?A' \subset ?B) |- _] =>  *)
+  (*   convertible A A'; extend (x \in B) ltac:(apply: (subsetP S)) *)
+  (* end. *)
+
 Local Notation pos := SetDef.pred_of_set.
 
-Ltac set_tab_step := 
+(* TODO: subsumption rules ??*)
+(* non-branching rules *)
+Ltac set_tab_close := 
   match goal with 
-  (* TODO: subsumption rules *)
-  (* non-branching rules *)
   | [H : is_true (_ \in pos (_ :&: _)) |- _] => 
     case/setIP : H => [? ?]
   | [H : is_true (?x \in _ (?A :\: ?B)) |- _] => 
@@ -30,11 +54,26 @@ Ltac set_tab_step :=
     notHyp (x \notin B); have ? : x \notin B by rewrite (disjointFr D H)
   | [H : is_true (?x \in ?A), S : is_true (?A \subset ?B) |- _] => 
     notHyp (x \in B); move/(subsetP S) : (H) => ?
-  (*branching rules *)
+  | [ H : ?A = ?B :> {set _} |- _] => 
+    move/eqP : H; rewrite eqEsubset => /andP [? ?]
+  | [ H : is_true (_ (_ :|: _) \subset _) |- _] => 
+    case/set_tac_subUl : H => [? ?]
+  | [ H : is_true (_ \subset _ (_ :&: _)) |- _] => 
+    case/set_tac_subIr : H => [? ?]
+  | [ H : is_true (_ [set _] \subset _) |- _] => 
+    rewrite sub1set in H
+  | [ xA : is_true (?x \in _ ?A), xB : is_true (?x \in _ ?B), 
+    H : is_true (_ (?A :&: ?B) \subset ?D) |- _] =>
+    notHyp (x \in D); have ? := set_tac_subIl xA xB H
+  end.
+
+(*branching rules *)
+Ltac set_tab_branch :=
+  match goal with 
   | [H : is_true (?x \in pos (?A :|: ?B)) |- _] => 
-    notHyp (x \in A); notHyp (x \in B); case/setUP : H => [?|?]
+    notHyp (x \in A); notHyp (x \in B); case/setUP : (H) => [?|?]
   | [ H : is_true (?x \notin pos (?A :&: ?B)) |- _] => 
-    notHyp (x \notin A); notHyp (x \notin B); case/setIPn : H => [?|?]
+    notHyp (x \notin A); notHyp (x \notin B); case/setIPn : (H) => [?|?]
   end.
 
 (** Note that the rules on disjointness and subset do not restricted
@@ -46,20 +85,33 @@ Ltac set_init :=
   | [ |- _ -> _] => intro;set_init
   | [ |- is_true (~~ _) ] => apply/negP => ?
   | [ |- is_true _] => apply: contraTT isT => ?
+  | [ |- _ ] => idtac (* nothing to be done here *)
   end.
+
+Ltac clean_mem := repeat match goal with [ H : _ |- _] => rewrite !mem_mem in H end.
                                           
-Ltac set_tac := subst; set_init; repeat (first [contrab|set_tab_step]).
+Ltac set_tac := set_init; subst; clean_mem;
+                repeat (first [contrab
+                              |set_tab_close
+                              |set_tab_branch]).
 
 (** Use typeclass inference to trigger set_tac using rewrite lemmas *)
 
 Class setBox (P : Prop) : Prop := SetBox { setBoxed : P }.
 Hint Extern 0 (setBox _) => apply SetBox; set_tac : typeclass_instances.
 
-Lemma inD (T : finType) (x : T) (A : {set T}) `{setBox (x \in A)} : x \in A. 
+Lemma inD (T : finType) (x : T) (A : pred T) `{setBox (x \in A)} : x \in A. 
 Proof. by case: H. Qed.
 
-Lemma notinD (T : finType) (x : T) (A : {set T}) `{setBox (x \notin A)} : x \notin A. 
+Lemma inD_debug (T : finType) (x : T) (A : pred T) : (x \in A) -> x \in A. 
+Proof. by []. Qed.
+
+Lemma notinD (T : finType) (x : T) (A : pred T) `{setBox (x \notin A)} : x \notin A. 
 Proof. by case: H. Qed.
+
+Lemma notinD_debug (T : finType) (x : T) (A : pred T) : (x \notin A) -> x \notin A. 
+Proof. by []. Qed.
+
 
 (* examples / unit tests *)
 
